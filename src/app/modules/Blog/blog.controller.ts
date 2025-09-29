b8652c857d0c26127f7c49752326b784a3d5427e
devsafix <devsafix@gmail.com>
2025-09-29 11:49:13 +0600

    Create: Update blog Controller

--- src/app/modules/Blog/blog.controller.ts
+++ src/app/modules/Blog/blog.controller.ts
@@ -45,8 +45,23 @@ const getSingleBlog = async (
   }
 };
 
+const updateBlog = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const result = await BlogService.updateBlog(id, req.body);
+    res.status(200).json({
+      success: true,
+      message: "Blog updated successfully!",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const BlogController = {
   createBlog,
   getAllBlogs,
   getSingleBlog,
+  updateBlog
 };
