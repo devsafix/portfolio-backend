22f66dc442d67bced6e47f96b5b78ea042877470
devsafix <devsafix@gmail.com>
2025-09-29 11:42:37 +0600

    Create: Auth middleware

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { Secret } from "jsonwebtoken";
+import config from "../../config";
+
+const auth = () => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // Get token from header
+    const token = req.cookies.accessToken;
+    if (!token) {
+      throw new Error("You are not authorized!");
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, config.jwt.secret as Secret);
+
+    // Attach user to request object
+    (req as any).user = decoded;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default auth;
