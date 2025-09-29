203fb4a3dc1a65676cb07d7eed3ea04a2844794e
devsafix <devsafix@gmail.com>
2025-09-29 11:29:04 +0600

    Create: Login controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -16,8 +16,22 @@ const registerOwner = async (req: Request, res: Response) => {
   }
 };
 
-
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const result = await AuthService.loginUser(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res
+      .status(401)
+      .json({ success: false, message: "Invalid credentials", error });
+  }
+};
 
 export const AuthController = {
   registerOwner,
+  loginUser,
 };
