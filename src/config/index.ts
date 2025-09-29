6a1ba9998d88e8cc60747bd7e9a633c994d89980
devsafix <devsafix@gmail.com>
2025-09-29 11:25:28 +0600

    Config: env file

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables from .env file at the root of the project
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+  },
+};
