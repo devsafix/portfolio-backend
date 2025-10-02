46ffc9c468fbbfe66aefca0231e8752678256967
devsafix <devsafix@gmail.com>
2025-10-02 16:03:37 +0600

    Add: me and logout routes

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { AuthController } from "./auth.controller";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/register", AuthController.registerOwner);
 router.post("/login", AuthController.loginUser);
+router.post("/logout", AuthController.logoutUser);
+
+router.get("/me", auth(), AuthController.getMyProfile);
 
 export const AuthRoutes = router;
