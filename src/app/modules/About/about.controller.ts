d8002ecd8432b999ef79c2e4899c219d57c62f9a
devsafix <devsafix@gmail.com>
2025-10-03 10:40:43 +0600

    Create: upsertAbout controller

---
+++ src/app/modules/About/about.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { AboutService } from "./about.service";
+
+const upsertAbout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AboutService.upsertAbout(req.body);
+    res.status(200).json({
+      success: true,
+      message: "About information updated successfully!",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+export const AboutController = {
+  upsertAbout,
+
+};
