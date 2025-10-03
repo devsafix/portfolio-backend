f86b78ffae5afeb5139d6fe1157656ed48bc1402
devsafix <devsafix@gmail.com>
2025-10-03 10:40:54 +0600

    Create: getAbout controller

--- src/app/modules/About/about.controller.ts
+++ src/app/modules/About/about.controller.ts
@@ -14,9 +14,20 @@ const upsertAbout = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-
+const getAbout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AboutService.getAbout();
+    res.status(200).json({
+      success: true,
+      message: "About information retrieved successfully!",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const AboutController = {
   upsertAbout,
-
+  getAbout,
 };
